84a961e16f4cab6bb84b7b79df2b51c985296c1e
Modupi <modupimaphoto@gmail.com>
2024-01-18 21:19:12 +0200

    4. Shell sort - Knuth Sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,5 +11,28 @@
  */
 void shell_sort(int *array, size_t size)
 {
+	unsigned int gap = 1, i, j;
+	int temp;
 
+	if (array == NULL || size < 2)
+		return;
+	while (gap <= size / 3)
+		gap = gap * 3 + 1;
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+		array[j] = temp;
+		}
+	gap /= 3;
+	print_array(array, size);
+	}
 }
