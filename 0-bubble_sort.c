761e312c90c2ea6147aaae67938b4310f986b18d
Modupi <modupimaphoto@gmail.com>
2024-01-18 14:48:55 +0200

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,5 +11,34 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i;
 
+	for (i = 0; i < size - 1; i++)
+	{
+		size_t j;
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+/**
+ * swap - first elemeent with second elememt.
+ *
+ * @a: first element.
+ * @b: second element.
+ *
+ * Return: Nothing.
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
