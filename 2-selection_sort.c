3111bebc8b287b306d6797f897572094987f29d8
Modupi <modupimaphoto@gmail.com>
2024-01-18 17:24:56 +0200

    2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,4 +11,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, index;
+	int tmp, swap, flag;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
+		}
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)
+			print_array(array, size);
+	}
 }
